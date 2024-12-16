0b5944e97dc3bea928070855555bd9aff7c6e67d
Dissang Tenkam Francis Kevin <dissangfrancis@yahoo.com>
2024-12-16 22:17:10 +0100

    update

--- lib/ui/product_category.dart
+++ lib/ui/product_category.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:myshop/constants/colors.dart';
 
 class ProductCategory extends StatelessWidget {
   final List<String> categories;
@@ -18,15 +17,18 @@ class ProductCategory extends StatelessWidget {
         itemCount: categories.length,
         itemBuilder: (context, index) {
           return Container(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-             decoration: BoxDecoration(
+            margin: const EdgeInsets.symmetric(horizontal: 4), // Espace entre les catégories
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12),
               border: Border.all(width: 1, color: Colors.black),
-             ),
-              child: Center(
-                child: Text(
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                getIconForCategory(categories[index]), // Icône personnalisée
+                const SizedBox(width: 8), // Espacement entre l'icône et le texte
+                Text(
                   categories[index],
                   textAlign: TextAlign.center,
                   style: const TextStyle(
@@ -35,11 +37,27 @@ class ProductCategory extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
+              ],
             ),
           );
         },
       ),
     );
+  }
+
+  // Méthode pour obtenir une icône personnalisée en fonction de la catégorie
+  Icon getIconForCategory(String category) {
+    switch (category) {
+      case 'All':
+        return const Icon(Icons.category, color: Colors.black, size: 16);
+      case 'SmartPhone':
+        return const Icon(Icons.smartphone, color: Colors.black, size: 16);
+      case 'HeadPhone':
+        return const Icon(Icons.headphones, color: Colors.black, size: 16);
+      case 'Laptop':
+        return const Icon(Icons.laptop, color: Colors.black, size: 16);
+      default:
+        return const Icon(Icons.device_unknown, color: Colors.black, size: 16); // Icône par défaut
+    }
   }
 }
